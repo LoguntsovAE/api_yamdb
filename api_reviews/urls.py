77e8b48c1c87d8a55901dffdc90bff3803b142f5
LoguntsovAE <loguntsovae@yandex.ru>
2021-02-16 17:35:28 +0500

    Исправлена ошибка со скобками в модели юзер. Подключены урлы приложения api_review

---
+++ api_reviews/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include 
+from django.urls import path 
+from rest_framework.routers import DefaultRouter 
+
+
+ 
+router = DefaultRouter() 
+
+# token = [ 
+#     path('', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
+#     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+# ] 
+ 
+urlpatterns = [
+]
