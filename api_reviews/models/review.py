bd0881eb3d1c86f8f13b49cf21771dd7aa679bc9
Дмитрий <flow.hack.com@gmail.com>
2021-02-16 11:59:22 +0400

    создана модель review

--- api_reviews/models/review.py
+++ api_reviews/models/review.py
@@ -1,5 +1,32 @@
 from django.db import models
+from .title import Title
+from .user import User
 
 
 class Review(models.Model):
-    pass
+    titles = models.ForeignKey(
+        Title,
+        verbose_name='К чему отзыв',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор комментария',
+        on_delete=models.CASCADE,
+        related_name='reviews'
+    )
+    text = models.TextField(
+        verbose_name='Текст комментария',
+        help_text='Напишите здесь текст вашего комментария!',
+    )
+    created = models.DateTimeField(
+        verbose_name='Дата создания комментария',
+        auto_now_add=True
+    )
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return f'<{self.author}> -> {self.text[:20]}'
