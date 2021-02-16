b78d58c527982edf8f67d397eea808330722ab74
Патимат Нажмудинова <patimat.n.m@yandex.ru>
2021-02-16 22:23:42 +0300

    Создать модели Title, Category, Genre

--- api_reviews/models/title.py
+++ api_reviews/models/title.py
@@ -1,5 +1,24 @@
 from django.db import models
 
+from .category import Category
+from .genre import Genre
+
 
 class Title(models.Model):
-    pass
+    name = models.CharField('Название',
+                            max_length=255)
+    author = models.CharField('Автор',
+                              max_length=100)
+    year = models.IntegerField('Год',
+                               max_length=4)
+    description = models.TextField('Описание')
+    genres = models.ManyToManyField(Genre,
+                                    verbose_name='Жанры')
+    category = models.ForeignKey(Category,
+                                 verbose_name='Категория',
+                                 on_delete=models.SET_NULL,
+                                 null=True)
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
