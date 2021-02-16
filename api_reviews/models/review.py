419dc8cfbce3b4dfc19bd4e4c08bd658b7ad5b5c
Дмитрий <flow.hack.com@gmail.com>
2021-02-16 16:43:17 +0400

    убран второй уровень комменатриев и убрана возможность несколько раз комментировать

--- api_reviews/models/review.py
+++ api_reviews/models/review.py
@@ -10,28 +10,26 @@ class Review(models.Model):
         on_delete=models.CASCADE,
         related_name='reviews'
     )
-    titles = models.ForeignKey(
+    title = models.ForeignKey(
         Title,
         verbose_name='К чему отзыв',
         on_delete=models.CASCADE,
         related_name='reviews'
     )
-    parent = models.ForeignKey(
-        'self',
-        verbose_name='Родительский комментарий',
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True,
-        related_name='replies'
-    )
     text = models.TextField(
         verbose_name='Текст комментария',
         help_text='Напишите здесь текст вашего комментария!',
     )
-    created = models.DateTimeField(
+    pub_date = models.DateTimeField(
         verbose_name='Дата создания комментария',
         auto_now_add=True
     )
 
     class Meta:
         ordering = ('-created',)
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=['title', 'author'], name='unique author'
+            )
+        ]
