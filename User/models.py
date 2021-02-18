2736800fcb554fb2e16a484671f771be4cbd7946
Дмитрий <flow.hack.com@gmail.com>
2021-02-18 12:02:54 +0400

    упрощена модель, добавлены роли

--- User/models.py
+++ User/models.py
@@ -1,88 +1,85 @@
-from django.core.mail import send_mail
+
 from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.base_user import BaseUserManager
 
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, **extra_fields):
-        """
-        Создание и сохранение юзера с данными параметрами
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_unusable_password()
-        user.save(using=self._db)
-        return user
-
     def create_user(self, email, **extra_fields):
         """
         Создание обычного пользователя
         """
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, **extra_fields)
+        if not email:
+            raise ValueError('Email обязателен')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_unusable_password()
+        user.save(using=self._db)
+
+        return user
 
     def create_superuser(self, email, **extra_fields):
         """
         Создание суперпользователя
         """
-        extra_fields.setdefault('is_superuser', True)
+        user = self.create_user(email)
+        user.role = 'admin'
+        user.set_unusable_password()
+        user.save(using=self._db)
 
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, **extra_fields)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     """
     Модель Юзера
     """
-    email = models.EmailField(_('email address'), unique=True)
+    class Role(models.TextChoices):
+        """
+        Роли
+        """
+        USER = 'user'
+        MODERATOR = 'moderator'
+        ADMIN = 'admin'
+
     username = models.CharField(
-        _('first name'),
-        max_length=30,
-        blank=True,
+        max_length=25,
+        unique=True,
+        verbose_name='username',
+        null=True
+    )
+    email = models.EmailField(
+        verbose_name='Почта',
         unique=True
     )
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
-    is_active = models.BooleanField(_('active'), default=True)
-    description = models.CharField(
-        _('description'), max_length=200, blank=True
+    role = models.CharField(
+        max_length=30,
+        verbose_name='Роль пользователя',
+        choices=Role.choices,
+        default=Role.USER
+    )
+    bio = models.TextField(
+        verbose_name='О себе',
+        null=True
+    )
+    first_name = models.CharField(
+        max_length=30,
+        verbose_name='Имя',
+        null=True
+    )
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name='Фамилия',
+        null=True
     )
 
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['email']
 
     class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-
-    def get_full_name(self):
-        """
-        Возвращает first_name и last_name в качестве полного имени
-        """
-        full_name = f'{self.first_name} {self.last_name}'
-        return full_name.strip()
-
-    def get_short_name(self):
-        """
-        Возвращает в качестве короткого имени first_name
-        """
-        return self.first_name
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        """
-        Как будет происходить отправка имейла
-        """
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+        ordering = ['username']
