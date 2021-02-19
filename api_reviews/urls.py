79031a4d463f816cac61d72b4a11a9aa2d71a884
LoguntsovAE <loguntsovae@yandex.ru>
2021-02-19 21:59:06 +0500

    Прописаны все урлы приложения api_reviews

--- api_reviews/urls.py
+++ api_reviews/urls.py
@@ -2,8 +2,22 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet)
+
 router_v1 = DefaultRouter()
 
-urlpatterns = [
+router_v1.register('titles', TitleViewSet)
+router_v1.register('categories', CategoryViewSet)
+router_v1.register('genres', GenreViewSet)
+router_v1.register('reviews', ReviewViewSet)
+router_v1.register('comments', CommentViewSet)
+router_v1.register('titles/(?<title_id>\d+)/reviews', ReviewViewSet)
+router_v1.register(
+    'titles/(?<title_id>\d+)/reviews/(?<review_id>\d+)/comments',
+    CommentViewSet
+)
 
+urlpatterns = [
+    path('v1/', include(router_v1.urls))
 ]
