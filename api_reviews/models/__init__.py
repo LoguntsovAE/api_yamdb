2e043fcd8a12b7d5810a5914019c907c60b1dbd3
Дмитрий <flow.hack.com@gmail.com>
2021-02-19 22:50:32 +0400

    Исправлены импорты в __init__.py и синтаксис settings.py, а также импорты в моделях

--- api_reviews/models/__init__.py
+++ api_reviews/models/__init__.py
@@ -1,3 +1,5 @@
 from .comment import Comment
 from .review import Review
 from .title import Title
+from .category import Category
+from .genre import Genre
--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -119,6 +119,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': [
         'rest_framework.pagination.PageNumberPagination',
-        ],
+    ],
     'PAGE_SIZE': 100
 }
