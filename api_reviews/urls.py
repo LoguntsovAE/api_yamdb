623c603ad72491b01093e1725880628f751675f1
LoguntsovAE <loguntsovae@yandex.ru>
2021-02-20 00:49:33 +0500

    Пофиксины баги синтаксиса и импортов

--- api_reviews/urls.py
+++ api_reviews/urls.py
@@ -2,24 +2,23 @@ from django.conf.urls import include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet, TitleViewSet)
+from .views import category, genre, comment, review, title
 
 router_v1 = DefaultRouter()
 
-router_v1.register('titles', TitleViewSet, basename='titles')
-router_v1.register('categories', CategoryViewSet, basename='categories')
-router_v1.register('genres', GenreViewSet, basename='genres')
-router_v1.register('reviews', ReviewViewSet, basename='reviews')
-router_v1.register('comments', CommentViewSet, basename='comments')
+router_v1.register('titles', title.TitleViewSet, basename='titles')
+router_v1.register('categories', category.CategoryViewSet, basename='categories')
+router_v1.register('genres', genre.GenreViewSet, basename='genres')
+router_v1.register('reviews', review.ReviewViewSet, basename='reviews')
+router_v1.register('comments', comment.CommentViewSet, basename='comments')
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews',
-    ReviewViewSet,
+    review.ReviewViewSet,
     basename='review'
 )
 router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet,
+    comment.CommentViewSet,
     basename='comment'
 )
 
--- api_reviews/views/review.py
+++ api_reviews/views/review.py
@@ -1,13 +1,20 @@
 from rest_framework import viewsets
-from rest_framework.permissions import (IsAuthenticated,
-                                        IsAuthenticatedOrReadOnly)
+from django.shortcuts import get_object_or_404
 
-# from api_reviews.models.review import Review
-# from .permissions import IsAuthorOrReadOnlyPermission
+from api_reviews.models import Review, Title
 from api_reviews.serializers.review import ReviewSerializer
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+
     serializer_class = ReviewSerializer
-    # permission_classes = [IsAuthenticated, IsAuthorOrReadOnlyPermission]
-    pass
+    queryset = Review.objects.all()
+
+    def perform_create(self, serializer):
+        get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, id=self.kwargs.get('title_id'))
+        return title.reviews
