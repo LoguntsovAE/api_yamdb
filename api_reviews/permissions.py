ac4ea3689916c9b61d7fddb0b81b74901e1873c8
Дмитрий <flow.hack.com@gmail.com>
2021-02-19 23:58:27 +0400

    Исправлен requirements.txt

--- api_reviews/permissions.py
+++ api_reviews/permissions.py
@@ -7,3 +7,12 @@ class IsAdminOrReadOnly(permissions.BasePermission):
             return True
         else:
             return request.user.is_staff
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    class IsAuthorOrReadOnly(permissions.BasePermission):
+        def has_object_permission(self, request, view, obj):
+            if request.method in permissions.SAFE_METHODS:
+                return True
+
+            return obj.author == request.user
