6f67d8b00abb58be710d69502c057cb1b9cfd5a5
LoguntsovAE <loguntsovae@yandex.ru>
2021-02-20 14:26:01 +0500

    Сделаны вьюхи Genre and Category

--- api_reviews/models/category.py
+++ api_reviews/models/category.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
 
     class Meta:
         verbose_name = 'Категория'
