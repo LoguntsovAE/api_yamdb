4451872e9351b20838de2c571290a3b14452bd51
Дмитрий <flow.hack.com@gmail.com>
2021-02-22 15:16:38 +0400

    Проведено ревью  иправлена фигня c JWT

--- api_reviews/permissions.py
+++ api_reviews/permissions.py
@@ -9,14 +9,6 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return request.user.is_staff
 
 
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj.author == request.user
-
-
 class IsOwnerOrReadOnlyPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.user == obj.author:
