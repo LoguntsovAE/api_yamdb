1b74ee3a1a9bcd018b911652d223a0c21f8250e4
Дмитрий <flow.hack.com@gmail.com>
2021-02-22 14:57:13 +0400

    Проведено ревью  иправлена фигня c JWT

--- User/models.py
+++ User/models.py
@@ -48,15 +48,11 @@ class User(AbstractUser):
 
     @property
     def is_admin(self):
-        return (
-                self.role == self.Role.ADMIN or self.is_superuser
-        )
+        return self.role == self.Role.ADMIN or self.is_superuser
 
     @property
     def is_moderator(self):
-        return (
-                self.is_admin or self.role == self.Role.MODERATOR
-        )
+        return self.is_admin or self.role == self.Role.MODERATOR
 
     def get_payload(self):
         return {
